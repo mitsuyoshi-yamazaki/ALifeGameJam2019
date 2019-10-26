2b0cdca37699943135d9d63305fb72c20a5b84fc
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-10-26 19:44:39 +0900

    p5.js の型定義ファイルを作成

---
+++ src/@types/p5/index.d.ts
@@ -0,0 +1,22 @@
+declare function stroke(r: number, g: number, b: number): void
+declare function ellipse(x: number, y: number, w: number, h: number): void
+
+// export const foobar = require('p5') as P5;
+
+// interface P5 {
+
+// }
+
+// export = module;
+// export as namespace p5;
+// declare global {
+// 	interface Window {
+// 		p5: typeof module,
+// 	}
+// }
+// interface IFooBar {
+// 	bazMethod(qux: string): string;
+// 	fuga(hoge: number): boolean
+// }
+// // tslint:disable-next-line:no-var-requires
+// const foobar = require('foobar') as IFooBar;
