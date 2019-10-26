c7a048427ada34942171f6da0051ec9352ca5abd
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-10-26 19:46:08 +0900

    コンパイルされたjsファイルを追加

--- out/main.js
+++ out/main.js
@@ -1,3 +1,7 @@
-let message = "Hello, TSSSSS!";
-console.log(message);
+function setup() {
+}
+function draw() {
+    stroke(0, 0, 255);
+    ellipse(50, 50, 80, 80);
+}
 //# sourceMappingURL=main.js.map
