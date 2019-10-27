1dd5a9dd9fdf380dbd5821d32eae0d66b60dca87
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-10-27 22:52:43 +0900

    Worldインターフェースを作成

--- out/main.js
+++ out/main.js
@@ -1,7 +1,28 @@
+let world;
 function setup() {
+    world = new VanillaWorld(200, 200);
 }
 function draw() {
-    stroke(0, 0, 255);
-    ellipse(50, 50, 80, 80);
+    world.next();
+    world.draw();
+}
+class VanillaWorld {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this._t = 0;
+    }
+    get t() {
+        return this._t;
+    }
+    next() {
+        this._t += 1;
+        // TODO: implement here
+    }
+    draw() {
+        // TODO: implement here
+        stroke(255, 0, 0);
+        ellipse(50, 50, 80, 80);
+    }
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
-
+let world: World
 
 function setup(): void {
+	world = new VanillaWorld(200, 200)
 }
 
 function draw(): void {
-	stroke(0, 0, 255)
-	ellipse(50, 50, 80, 80);
+	world.next()
+	world.draw()
+}
+
+// TODO: ファイルを分割する
+interface World {
+	t: number
+	next(): void
+	draw(): void	// TODO: 世界は描画系とは独立して存在するので、描画処理は外部に出して、 World.state を読み込み描画するようにする
+}
+
+class VanillaWorld implements World {
+	private _t: number = 0
+	get t(): number {
+		return this._t
+	}
+
+	constructor(readonly width: number, readonly height: number) {
+	}
+
+	next(): void {
+		this._t += 1
+		// TODO: implement here
+	}
+
+	draw(): void {
+		// TODO: implement here
+		stroke(255, 0, 0)
+		ellipse(50, 50, 80, 80);
+	}
 }
