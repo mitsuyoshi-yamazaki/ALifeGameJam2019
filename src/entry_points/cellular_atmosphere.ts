27df5b6425bdfcc3302203804ca25f033655a2c7
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-12-14 21:57:24 +0900

    気圧が均一化する

--- src/entry_points/cellular_atmosphere.ts
+++ src/entry_points/cellular_atmosphere.ts
@@ -1,36 +1,46 @@
 import * as p5 from "p5"
 import { random } from "../utilities"
 
-const main = (p: p5) => {
-  let states: State[][] = []
-  const size = 100
-  const cellSize = 10
-  const maxPressure = 20
+let t = 0
+const cells: Cell[][] = []
+const size = 6
+const cellSize = 1000 / size
+const maxPressure = 1000
+const radius = 1
+const diameter = radius * 2 + 1
+const numberOfNeighbors = (radius + 1) * radius * 4
 
+const main = (p: p5) => {
   p.setup = () => {
     const fieldSize = size * cellSize
     p.createCanvas(fieldSize, fieldSize)
-    setupStates()
+    setupCells()
   }
 
   p.draw = () => {
+    if (t % 60 !== 0) {
+      t += 1
+
+      return
+    }
     update()
     draw()
+    t += 1
   }
 
   function draw(): void {
     p.noStroke()
     const white = p.color(255)
 
-    for (let y = 0; y < states.length; y += 1) {
-      const row = states[y]
+    for (let y = 0; y < cells.length; y += 1) {
+      const row = cells[y]
       for (let x = 0; x < row.length; x += 1) {
-        const state = row[x]
+        const cell = row[x]
         const xx = x * cellSize
         const yy = y * cellSize
 
-        const progress = Math.min(state.pressure / maxPressure, 1)
-        const color = p.lerpColor(white, state.color(p), progress)
+        const progress = Math.min(cell.currentState.pressure / maxPressure, 1)
+        const color = p.lerpColor(white, cell.currentState.color(p), progress)
         p.fill(color)
         p.rect(xx, yy, cellSize, cellSize)
       }
@@ -38,25 +48,61 @@ const main = (p: p5) => {
   }
 
   function update(): void {
-    for (let y = 0; y < states.length; y += 1) {
-      const row = states[y]
+    cells.forEach(row => {
+      row.forEach(cell => {
+        cell.next()
+      })
+    })
+
+    for (let y = 0; y < cells.length; y += 1) {
+      const row = cells[y]
       for (let x = 0; x < row.length; x += 1) {
-        const state = row[x]
+        const cell = row[x]
+
+        const neighbourCells: Cell[] = []
+        for (let j = -radius; j <= radius; j += 1) {
+          for (let i = -radius; i <= radius; i += 1) {
+            if ((i === 0) && (j === 0)) {
+              continue
+            }
+            const neighbour = cells[(j + size) % size][(i + size) % size]
+            neighbourCells.push(neighbour)
+          }
+        }
+
+        const cellsWithSameMaterial = neighbourCells.filter(c => {
+          return c.currentState.material === cell.currentState.material
+        })
+
+        cellsWithSameMaterial.forEach(neighbour => {
+          if (neighbour.currentState.pressure > cell.currentState.pressure) { // 絶対値の小さい方に floor しなければ気圧が負数をとりうるため
+            const pressureDifference = Math.floor((neighbour.currentState.pressure - cell.currentState.pressure) / numberOfNeighbors)
+            if (pressureDifference < 1) {
+              return
+            }
+            cell.nextState.pressure += pressureDifference
+          } else {
+            const pressureDifference = Math.floor((cell.currentState.pressure - neighbour.currentState.pressure) / numberOfNeighbors)
+            if (pressureDifference < 1) {
+              return
+            }
+            cell.nextState.pressure -= pressureDifference
+          }
+        })
       }
     }
   }
 
-  function setupStates(): void {
-    states = []
+  function setupCells(): void {
     for (let y = 0; y < size; y += 1) {
-      const row: State[] = []
+      const row: Cell[] = []
       for (let x = 0; x < size; x += 1) {
-        // row.push(State.random())
         const state = new State()
-        state.pressure = random(maxPressure, 0)
-        row.push(state)
+        state.pressure = Math.floor(random(maxPressure))
+        const cell = new Cell(state)
+        row.push(cell)
       }
-      states.push(row)
+      cells.push(row)
     }
   }
 }
@@ -89,9 +135,46 @@ function colorOf(material: Material, p: p5): p5.Color {
   }
 }
 
+class Cell {
+  public get currentState(): State {
+    return this._currentState
+  }
+  public get nextState(): State {
+    return this._nextState
+  }
+  private _currentState: State
+  private _nextState: State
+
+  public constructor(state: State) {
+    this._currentState = state
+    this._nextState = this.currentState.clone()
+  }
+
+  public static random(): Cell {
+    return new Cell(State.random())
+  }
+
+  public next(): void {
+    this._currentState = this.nextState
+    this._nextState = this.currentState.clone()
+  }
+}
+
 class State {
+  public static minimumPressure = 1
   public material = Material.Hydrogen
-  public pressure = 0
+  private _pressure = 0
+  public set pressure(value: number) {
+    // if (value <= 0) {
+    //   this._pressure = 0
+
+    //   return
+    // }
+    this._pressure = value
+  }
+  public get pressure(): number {
+    return this._pressure
+  }
 
   public static random(): State {
     const state = new State()
@@ -101,17 +184,34 @@ class State {
       Material.Nitrogen,
       Material.CarbonDioxide,
     ]
-    const maxPressure = 20
 
     state.material = materials[Math.floor(random(materials.length))]
 
     const isVacuum = state.material === Material.Vacuum
-    state.pressure = isVacuum ? 0 : random(maxPressure) + 1
+    state.pressure = isVacuum ? 0 : Math.floor(random(maxPressure))
 
     return state
   }
 
   public color(p: p5): p5.Color {
-      return colorOf(this.material, p)
+    return colorOf(this.material, p)
+  }
+
+  public clone(): State {
+    const state = new State()
+    state.material = this.material
+    state.pressure = this.pressure
+
+    return state
   }
 }
+
+function pressureOf(state: State, surroundings: State[]): number {
+   return 0	// TODO: materialが異なれば与えるpressureが異なる？
+}
+
+// function transference(to: State, from: State, pressure): number {
+//   if (to.material != ) {
+
+//     }
+// }
