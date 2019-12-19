b68e166b61b6ce79003367440c25544dbfeef4c8
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-12-19 22:56:53 +0900

    ファイルを追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		main: './src/entry_points/main.ts',
 		gravitational_field: './src/entry_points/gravitational_field.ts',
 		orbital_motion: './src/entry_points/orbital_motion.ts',
+		no_friction: './src/entry_points/no_friction.ts',
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
