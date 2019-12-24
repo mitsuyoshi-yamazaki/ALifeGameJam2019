846b429e1ef16126e763a2746e760ce39895a7a8
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-12-25 01:03:41 +0900

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		main: './src/entry_points/main.ts',
 		gravitational_field: './src/entry_points/gravitational_field.ts',
 		orbital_motion: './src/entry_points/orbital_motion.ts',
+		generative_art_002: './src/entry_points/generative_art_002.ts',
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
