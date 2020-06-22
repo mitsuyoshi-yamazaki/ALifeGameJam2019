88e71489e5998a02ae84eb03d69e010827f90888
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2020-06-22 20:54:26 +0900

    描画処理を実装

--- src/entry_points/evo_devo.ts
+++ src/entry_points/evo_devo.ts
@@ -7,7 +7,9 @@ const parameters = parsedQueries()
 const DEBUG = parameters["debug"] ? true : false  // Caution: 0 turns to "0" and it's true. Use "" to disable it.
 const size = parameters["size"] ? parseInt(parameters["size"], 10) : 1000
 const rawRules = parameters["rules"]  // rules=A:-C+B+C,B:A
+const rawConstants = parameters["constants"]  // constants=+:55,-:-55
 const speed = parameters["speed"] ? parseInt(parameters["speed"], 10) : 1
+const unitLength = parameters["length"] ? parseInt(parameters["length"], 10) : 100
 // tslint:enable: no-string-literal
 
 const canvasSize = new Vector(size, size)
@@ -27,7 +29,9 @@ const main = (p: p5) => {
     canvas.id("canvas")
     canvas.parent("canvas-parent")
 
-    node = new Node(parseRules(rawRules), undefined, "A")
+    const system = new LSystem(parseRules(rawRules), parseConstants(rawConstants))
+    const position = new Vector(canvasSize.x * 0.5, canvasSize.y * 0.9)
+    node = new Node(system, undefined, "A", position, -90, 0)
   }
 
   p.draw = () => {
@@ -62,9 +66,43 @@ function parseRules(raw: string | undefined): Map<string, string> {
   return map
 }
 
+function parseConstants(raw: string | undefined): Map<string, number> {
+  const map = new Map<string, number>()
+  if (raw == undefined) {
+    console.log(`No constant specified`)
+    map.set("+", 20)
+    map.set("-", -20)
+
+    return map
+  }
+  const rawRuleSet = raw.split(",")
+  rawRuleSet.forEach(line => {
+    const keyValue = line.split(":")
+    if (keyValue.length !== 2) {
+      console.log(`[Warning] Parameter "constants" line "${line}" should be "<character>:<number>"`)
+
+      return
+    }
+    const angle = parseInt(keyValue[1], 10)
+    if (angle === undefined) {
+      console.log(`[Warning] Parameter "constants" line "${line}" should be "<character>:<number>"`)
+
+      return
+    }
+    map.set(keyValue[0], angle)
+  })
+
+  return map
+}
+
 function step(): void {
   log(node.fullState())
   node.step()
+}
+
+class LSystem {
+  public constructor(public readonly rules: Map<string, string>, public readonly constants: Map<string, number>) {
+  }
 }
 
 class Node {
@@ -74,8 +112,16 @@ class Node {
   public readonly children: Node[] = []
   private readonly _state: string
   private matured = false
-  public constructor(public readonly rules: Map<string, string>, public readonly parent: Node | null, initialState: string) {
-    this._state = initialState
+  public constructor(
+    public readonly system: LSystem,
+    public readonly parent: Node | null,
+    state: string,
+    public readonly position: Vector,
+    public readonly direction: number,
+    public readonly depth: number,
+  ) {
+    this._state = state
+    // log(`[${depth}] ${state}: ${String(position)}`)
   }
 
   public fullState(): string {
@@ -94,10 +140,22 @@ class Node {
   public step(): void {
     if (this.matured === false) {
       this.matured = true
-      const nextCondition = this.rules.get(this.state)
+      const nextCondition = this.system.rules.get(this.state)
       if (nextCondition != undefined) {
+        let newDirection = this.direction
+        const length = (1 / (this.depth + 1)) * unitLength
+
         for (const c of nextCondition) {
-          this.children.push(new Node(this.rules, this, c))
+          const directionChange = this.system.constants.get(c)
+          if (directionChange != undefined) {
+            newDirection += directionChange
+            continue
+          }
+
+          const radian = newDirection * (Math.PI / 180)
+          const nextPosition = this.position.moved(radian, length)
+
+          this.children.push(new Node(this.system, this, c, nextPosition, newDirection, this.depth + 1))
         }
       }
     } else {
@@ -108,7 +166,12 @@ class Node {
   }
 
   public draw(p: p5): void {
-    // TODO:
+    if (this.parent) {
+      p.strokeWeight(1)
+      p.noFill()
+      p.stroke(0xFF, 0xA0)
+      p.line(this.parent.position.x, this.parent.position.y, this.position.x, this.position.y)
+    }
 
     this.children.forEach(childNode => {
       childNode.draw(p)
@@ -134,43 +197,6 @@ class Node {
 
     return result
   }
-
-  // private recursiveDraw(p: p5, condition: string, position: Vector, direction: number, depth: number, previousCenter: Vector | undefined): void {
-  //   if (depth < 1) {
-  //     return
-  //   }
-
-  //   let newDirection = direction
-  //   const length = Math.pow(0.9, maxDepth - depth) * unitLength
-
-  //   for (const c of condition) {
-  //     const directionChange = this.constants.get(c)
-  //     if (directionChange != undefined) {
-  //       newDirection += directionChange
-  //       continue
-  //     }
-
-  //     const radian = newDirection * (Math.PI / 180)
-  //     const nextPosition = position.moved(radian, length)
-  //     // p.line(position.x, position.y, nextPosition.x, nextPosition.y)
-  //     const center = nextPosition.add(position)
-  //       .div(2)
-  //     p.strokeWeight(1)
-  //     if (drawCircle) {
-  //       p.circle(center.x, center.y, length * 0.5)
-  //     }
-  //     if (drawLine && (previousCenter != undefined)) {
-  //       p.strokeWeight(0.5)
-  //       p.line(center.x, center.y, previousCenter.x, previousCenter.y)
-  //     }
-
-  //     const nextCondition = this.rules.get(c)
-  //     if (nextCondition != undefined) {
-  //       this.recursiveDraw(p, nextCondition, nextPosition, newDirection, depth - 1, center)
-  //       continue
-  //     }
-  //   }
-  // }
 }
 
 const sketch = new p5(main)
