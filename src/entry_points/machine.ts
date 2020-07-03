8c788ffca5778308ef9163c34ab6dbb3cf8d9360
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2020-07-03 21:38:43 +0900

    最低限の機能を実装

--- src/entry_points/machine.ts
+++ src/entry_points/machine.ts
@@ -1,6 +1,11 @@
 import * as p5 from "p5"
 import { parsedQueries, random } from "../utilities"
 
+/**
+ * TODO:
+ * iをランダムな位置から検索し、子孫数を1未満にする
+ */
+
 const parameters = parsedQueries()
 // tslint:disable: no-string-literal
 const rawDebug = parseInt(parameters["debug"], 10)
@@ -8,6 +13,11 @@ const DEBUG = isNaN(rawDebug) ? false : (rawDebug ? true : false)
 const rawTest = parseInt(parameters["test"], 10)
 const TEST = isNaN(rawTest) ? false : (rawTest ? true : false)
 const size = parameters["size"] ? parseInt(parameters["size"], 10) : 100
+const machineCount = parameters["machines"] ? parseInt(parameters["machines"], 10) : 100
+const machineMax = parameters["max"] ? parseInt(parameters["max"], 10) : 1000
+const matingRate = parameters["mating_rate"] ? parseFloat(parameters["mating_rate"]) : 0.1
+const mutationRate = parameters["mutation_rate"] ? parseFloat(parameters["mutation_rate"]) : 0.03
+const speed = parameters["speed"] ? parseInt(parameters["speed"], 10) : 300
 // tslint:enable: no-string-literal
 
 function log(message: string): void {
@@ -16,7 +26,9 @@ function log(message: string): void {
   }
 }
 
+let tt = -1
 let t = 0
+let machines: Machine[] = []
 
 const main = (p: p5) => {
   p.setup = () => {
@@ -28,15 +40,97 @@ const main = (p: p5) => {
     if (TEST) {
       tests()
     }
+
+    for (let i = 0; i < machineCount; i += 1) {
+      machines.push(new Machine(new Gene(0b1100111100)))
+    }
   }
 
   p.draw = () => {
+    tt += 1
+    if (tt % speed !== 0) {
+      return
+    }
+
+    next()
     t += 1
     setTimestamp(t)
   }
 }
 
 const sketch = new p5(main)
+
+function next(): void {
+  const trimmed = trimMachines()
+  const born = reproduceMachines()
+  showStatistics(born, trimmed)
+}
+
+function reproduceMachines(): number {
+  const newMachines: Machine[] = []
+
+  for (let i = 0; i < machines.length; i += 1) {
+    const machine = machines[i]
+
+    for (let j = i + 1; j < machines.length; j += 1) {
+      if (random(1) > matingRate) {
+        continue
+      }
+      const other = machines[j]
+      const offsprings = machine.reproduce(other)
+      newMachines.push(...offsprings)
+    }
+  }
+
+  machines.push(...newMachines)
+
+  return newMachines.length
+}
+
+function trimMachines(): number {
+  if (machines.length <= machineMax) {
+    return 0
+  }
+
+  const trimmed: Machine[] = []
+  for (let i = 0; i < machines.length; i += 1) {
+    if ((machines.length - trimmed.length) <= machineMax) {
+      break
+    }
+
+    const machine = machines[i]
+    if (machine.age > 0) {  // machines は先頭から古い順に並ぶため // TODO: 子孫を多く残したものを優先して残す
+      trimmed.push(machine)
+    }
+  }
+
+  machines = machines.filter(m => trimmed.indexOf(m) === -1)
+
+  return trimmed.length
+}
+
+function showStatistics(born: number, trimmed: number): void {
+  const genesMap = new Map<number, number>() // {gene: number of genes}
+
+  log(`\n\n\n[${t}]\nMachines: ${machines.length}, ${born} born, ${trimmed} die`)
+
+  machines.forEach(m => {
+    // tslint:disable-next-line: strict-boolean-expressions
+    const numberOfMachines = genesMap.get(m.gene.value) || 0
+    genesMap.set(m.gene.value, numberOfMachines + 1)
+  })
+
+  const genes: [number, number][] = []
+  genesMap.forEach((value, gene) => {
+    genes.push([gene, value])
+  })
+
+  const sorted = genes.sort((lhs, rhs) => rhs[1] - lhs[1])
+  sorted.slice(0, Math.min(sorted.length, 5))
+    .forEach(e => {
+      log(`${e[0].toString(2)}: ${e[1]}`)
+    })
+}
 
 /**
  * geneLength bits binary
@@ -58,6 +152,13 @@ class Gene {
     const upper = rawTransitionTable << Gene.headerLength
     const lower = rawTransitionTable >> (Gene.transitionTableLength - Gene.headerLength)
     this.transitionTable = upper + lower
+  }
+
+  public mutated(): Gene {
+    const mutation = 1 << Math.floor(random(Gene.geneLength, 0))
+    const mutatedValue = this.value ^ mutation
+
+    return new Gene(mutatedValue)
   }
 
   public reproduce(other: Gene): Gene[] {
@@ -85,7 +186,25 @@ class Gene {
 }
 
 class Machine {
+  public createdAt: number
+  public get age(): number {
+    return t - this.createdAt
+  }
 
+  public constructor(public readonly gene: Gene) {
+    this.createdAt = t
+  }
+
+  public reproduce(other: Machine): Machine[] {
+    return this.gene.reproduce(other.gene)
+      .map(g => {
+        if (random(1) < mutationRate) {
+          return new Machine(g.mutated())
+        } else {
+          return new Machine(g)
+        }
+      })
+  }
 }
 
 function assert(b: boolean, message: string): void {
