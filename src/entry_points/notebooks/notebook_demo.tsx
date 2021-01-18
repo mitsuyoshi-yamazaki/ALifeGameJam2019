7765e4dc4b41306b9687b37ba6bc2d6c21e89b72
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-01-18 21:50:32 +0900

    canvas表示領域を実装

--- src/entry_points/notebooks/notebook_demo.tsx
+++ src/entry_points/notebooks/notebook_demo.tsx
@@ -1,18 +1,12 @@
 import React, { CSSProperties } from "react"
 import ReactDOM from "react-dom"
 
-interface TextSectionProps {
-  title: string
-  description: string
-}
-
 const Section: React.FunctionComponent<{}> = props => {
   const sectionStyle: CSSProperties = {
     border: "1px solid",
     borderRadius: "5px",
     borderColor: "lightgrey",
     marginTop: "2rem",
-    padding: "1rem",
   }
 
   return (
@@ -22,15 +16,61 @@ const Section: React.FunctionComponent<{}> = props => {
   )
 }
 
+interface TextSectionProps {
+  title: string
+  description: string
+}
+
 const TextSection: React.FunctionComponent<TextSectionProps> = props => {
+  const style: CSSProperties = {
+    padding: "1rem",
+  }
+
   return (
-    <div>
+    <div style={style}>
       <h2>{props.title}</h2>
       <div>
         <p>{props.description}</p>
       </div>
     </div>
   )
+}
+
+interface CanvasSectionProps {
+  src: string
+}
+
+class CanvasSection extends React.Component<CanvasSectionProps> {
+  private readonly p5Id = "script-p5"
+  private readonly simulationId = "script-simulation"
+
+  public componentDidMount() {
+    // https://qiita.com/m3816/items/36e5ce2351f3093f2402
+    this.replaceScriptElement(this.p5Id, "../../p5.min.js")
+    this.replaceScriptElement(this.simulationId, this.props.src)
+  }
+
+  public render() {
+    return (
+      <div>
+        <script id={this.p5Id}></script>
+        <script id={this.simulationId}></script>
+        <div id="canvas-parent" />
+      </div>
+    )
+  }
+
+  private replaceScriptElement(id: string, src: string) {
+    const scriptElement = document.createElement("script")
+
+    scriptElement.src = src
+
+    const element = document.getElementById(id)
+    if (element == undefined) {
+      return
+    }
+    element.replaceWith(scriptElement)
+  }
 }
 
 const App = () => {
@@ -47,6 +87,9 @@ const App = () => {
   return (
     <div style={constentStyle}>
       <h1 style={titleStyle}>BlindPainter</h1>
+      <Section>
+        <CanvasSection src="https://cdn.jsdelivr.net/gh/mitsuyoshi-yamazaki/ALifeGameJam2019@0e7f4c5f63cdd010599f0c1c6fc09cd2ff05c68f/dist/machine.js"/>
+      </Section>
       <Section>
         <TextSection title="はじめに" description="BlindPainterはALife Game Jam 2019で作成した人工生命系/ジェネラティブアートです。作品のコンセプトを 収束しない進化 と設定し、常に移り変わって寡占にならない人工生命系を実装しました。" />
       </Section>
